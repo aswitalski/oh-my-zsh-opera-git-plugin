a70189d4a50a625754c9cbeca15e3c9ed1201f95
Aleksander <aleksander.switalski@gmail.com>
2016-09-28 17:59:46 +0200

    Create opera-git.plugin.zsh

---
+++ opera-git.plugin.zsh
@@ -0,0 +1,35 @@
+function work_package() {
+ echo $(echo $(current_branch) | cut -d'/' -f 2)
+}
+
+function prev_branch() {
+  ordinal=$(echo $(current_branch) | cut -d'/' -f 3)
+  echo 'wp/'$(work_package)'/'$(($ordinal - 1))
+}
+
+function next_branch() {
+  ordinal=$(echo $(current_branch) | cut -d'/' -f 3)
+  echo 'wp/'$(work_package)'/'$(($ordinal + 1))
+}
+
+function latest_nightly_build() {
+  echo $(git log -50 --pretty=format:"%sź%H" | grep 'buildbot: update nightly version number' | cut -d'ź' -f 2 | head -1)
+}
+
+function commit_with_msg() {
+  echo "git commit -m '$(work_package): $1'"
+}
+
+alias gcobn='git checkout -b $(next_branch)'
+alias gcop='git checkout $(prev_branch)'
+alias gcon='git checkout $(next_branch)'
+alias grin='git rebase -i $(latest_nightly_build)'
+alias grins='git rebase -i --autosquash $(latest_nightly_build)'
+
+function gcwp() {
+  echo $(commit_with_msg $1) | zsh
+}
+
+function gcobwp() {
+  echo "git checkout -b 'wp/$1/1'" | zsh
+}
