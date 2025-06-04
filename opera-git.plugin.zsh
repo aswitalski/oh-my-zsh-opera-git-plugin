fd9b8ae82880aab679d8fcfa06ec96db3e48b00a
Aleksander Świtalski <aswitalski@opera.com>
2025-06-04 13:56:16 +0200

    Add new aliases and functions

--- opera-git.plugin.zsh
+++ opera-git.plugin.zsh
@@ -20,8 +20,78 @@ function latest_wp_branch() {
   echo $(git branch -a | grep $1 | grep -v remotes | tail -r | head -1)
 }
 
+function my_last_commits {
+  git log -1000 --pretty=format:"%ad | %an | %s | %h" --date=iso |
+    grep "Aleksander" |
+    while IFS= read -r line; do
+      format_commit_line "$line"
+    done
+}
+
+# Color helpers
+green()  { echo -n "\033[32m$1\033[0m"; }
+yellow() { echo -n "\033[33m$1\033[0m"; }
+cyan()   { echo -n "\033[96m$1\033[0m"; }
+gray()   { echo -n "\033[90m$1\033[0m"; }
+
+format_commit_line() {
+  local line="$1"
+  local URL_BASE="https://bugs.opera.com/browse/"
+
+  local date name message hash
+  IFS='|' read -r date name message hash <<< "$line"
+
+  # Clean and isolate ISO date
+  date="${date%% *}"  # Remove time
+  local yyyy="${date:0:4}"
+  local mm="${date:5:2}"
+  local dd="${date:8:2}"
+
+  # Convert month number to name
+  local month
+  case "$mm" in
+    "01") month="Jan" ;;
+    "02") month="Feb" ;;
+    "03") month="Mar" ;;
+    "04") month="Apr" ;;
+    "05") month="May" ;;
+    "06") month="Jun" ;;
+    "07") month="Jul" ;;
+    "08") month="Aug" ;;
+    "09") month="Sep" ;;
+    "10") month="Oct" ;;
+    "11") month="Nov" ;;
+    "12") month="Dec" ;;
+  esac
+
+  local formatted_date="$dd $month $yyyy"
+
+  # Trim whitespace from fields
+  name="${name#"${name%%[![:space:]]*}"}"
+  name="${name%"${name##*[![:space:]]}"}"
+  message="${message#"${message%%[![:space:]]*}"}"
+  message="${message%"${message##*[![:space:]]}"}"
+  hash="${hash#"${hash%%[![:space:]]*}"}"
+  hash="${hash%"${hash##*[![:space:]]}"}"
+
+  # Extract issue ID and commit message
+  local id=""
+  local msg="$message"
+  local link=""
+  if [[ "$message" =~ ^([A-Z]+\-[0-9]+):[[:space:]]*(.*)$ ]]; then
+    id="${match[1]}"
+    msg="${match[2]}"
+    link=$'\e]8;;'"${URL_BASE}${id}"$'\a'"$(cyan "$id")"$'\e]8;;\a'
+  fi
+
+  # Final output
+  echo -e "$(gray "==>") $formatted_date $(gray "-") $link: $msg $(gray "-") $(yellow "$hash") "
+}
+
 function commit_with_msg() {
-  echo "git commit -m '$(work_package): $1'"
+  local msg="$1"
+  shift
+  echo "git commit -m '$(work_package): $msg' $@"
 }
 
 alias gfsu='cd chromium/src/ && gaa && git reset --hard && cd ... && gsu'
@@ -32,14 +102,28 @@ alias gcon='git checkout $(next_branch)'
 alias grin='git rebase -i $(latest_nightly_build)'
 alias grins='git rebase -i --autosquash $(latest_nightly_build)'
 
+alias glc='my_last_commits'
+
 alias jira='$(echo "open https://bugs.opera.com/browse/$(work_package)")'
 
-function gcwp() {
-  echo $(commit_with_msg $1) | zsh
+function gcobwp() {
+  # $1 is the passed task ID
+  local task_id="$1"
+
+  # Call your existing function to retrieve current branch name
+  local cur_branch
+  cur_branch=$(current_branch)  # or however you implement current_branch
+
+  # Check if current branch starts with "desktop-stable" or "gx-stable"
+  if [[ "$cur_branch" == desktop-stable* || "$cur_branch" == gx-stable* ]]; then
+    git checkout -b "wp/${task_id}/${cur_branch}/1"
+  else
+    git checkout -b "wp/${task_id}/1"
+  fi
 }
 
-function gcobwp() {
-  echo "git checkout -b 'wp/$1/1'" | zsh
+function gcwp() {
+  echo "$(commit_with_msg "$@")" | zsh
 }
 
 function gcowp() {
